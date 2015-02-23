bb75fd8317fcef8a230f5c8aefada2b20aeb9d54
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-02-23 09:03:47 +0200

    bugfix

--- bnt_tv.sh
+++ bnt_tv.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+pageUrl='http://cdn.bg/live/4eViE8vGzI'
 exec rtmpdump\
   --rtmp     'rtmp://edge3.cdn.bg:2020/fls'\
   --app      'fls'\
   --flashVer 'LNX 11,8,800,96'\
   --swfVfy   'http://cdn.bg/eflash/jwplayer510/player.swf'\
-  --pageUrl  'http://cdn.bg/live/4eViE8vGzI'\
-  --playpath 'bnt.stream?at=2c2e68beb8a99e014f8e12e785107b05'\
+  --pageUrl  "$pageUrl"\
+  --playpath "bnt.stream?at=$(curl -s "$pageUrl" | grep -oP 'bnt.stream\?at=\K\w+')"\
   --token    'B@1R1st1077'\
   --live --flv - --quiet | cvlc --file-caching 60000\
   --key-play-pause ''\
