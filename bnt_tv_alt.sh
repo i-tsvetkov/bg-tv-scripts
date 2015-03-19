fa3e155feb806ccb3655857a19e420f18771011b
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-03-20 00:56:44 +0200

    added referer to prevent blocking

--- bnt_tv_alt.sh
+++ bnt_tv_alt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 pageUrl='http://cdn.bg/live/4eViE8vGzI'
 exec rtmpdump\
-  --rtmp     'rtmp://edge3.cdn.bg:2020/fls'\
+  --rtmp     'rtmp://edge11.cdn.bg:2020/fls'\
   --app      'fls'\
   --flashVer 'LNX 11,8,800,96'\
   --swfVfy   'http://cdn.bg/eflash/jwplayer510/player.swf'\
   --pageUrl  "$pageUrl"\
-  --playpath "bnt.stream?at=$(curl -s "$pageUrl" | grep -oP 'bnt.stream\?at=\K\w+')"\
+  --playpath "bnt.stream?at=$(curl -s -H 'Referer: http://tv.bnt.bg/bnt1/16x9/' "$pageUrl" | grep -oP 'bnt.stream\?at=\K\w+')"\
   --token    'B@1R1st1077'\
   --live --flv - --quiet | mpv --cache=10000\
   --cache-min=10\
