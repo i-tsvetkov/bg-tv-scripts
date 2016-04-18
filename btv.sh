91530b10da8380574497d058553db73cb59bb820
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-04-18 20:35:43 +0300

    update fix

--- btv.sh
+++ btv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 exec rtmpdump\
-  --rtmp     'rtmp://46.10.150.113:80/ios'\
-  --app      'ios'\
+  --rtmp     'rtmp://hls.btv.bg.sof.cmestatic.com:80/alpha'\
+  --app      'alpha'\
   --flashVer 'LNX 11,8,800,96'\
-  --swfVfy   'http://images.btv.bg/fplayer/flowplayer.commercial-3.2.5.swf'\
+  --swfVfy   'http://www.btv.bg/static/bg/shared/app/flowplayer/flowp.layer.commercial-3.2.18.swf'\
   --pageUrl  'http://www.btv.bg/live/'\
-  --playpath 'btvbglive'\
+  --playpath 'alpha'\
   --live --flv - --quiet | cvlc --file-caching 60000\
   --key-play-pause ''\
   --key-next ''\
