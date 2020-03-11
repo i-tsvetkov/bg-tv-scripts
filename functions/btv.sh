026203988ad3239e164c960e68b36fb5c80f0cc9
Ito <ito@localhost>
2020-03-11 15:01:21 +0200

    refactoring

---
+++ functions/btv.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euxo pipefail
+
+function btv() {
+  cookiejar="$(mktemp '/tmp/tmp.btv.cookiejar.XXXXXXXXXX')"
+
+  curl 'https://btvplus.bg/lbin/social/login.php' \
+    --fail \
+    --silent \
+    -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0' \
+    -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+    -H 'Accept-Language: en-US,en;q=0.5' \
+    -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+    -H 'X-Requested-With: XMLHttpRequest' \
+    -H 'Origin: https://btvplus.bg' \
+    -H 'DNT: 1' \
+    -H 'Connection: keep-alive' \
+    -H 'Referer: https://btvplus.bg/live/' \
+    -H 'Pragma: no-cache' \
+    -H 'Cache-Control: no-cache' \
+    --compressed \
+    --data "username=${BTV_USERNAME}&password=${BTV_PASSWORD}" \
+    --cookie-jar "$cookiejar"
+
+  timestamp_in_milliseconds="$(date '+%s%3N')"
+
+  url="$(
+    curl "https://btvplus.bg/lbin/v3/btvplus/player_config.php?media_id=2110383625&_=${timestamp_in_milliseconds}" \
+      --fail \
+      --silent \
+      -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0' \
+      -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+      -H 'Accept-Language: en-US,en;q=0.5' \
+      -H 'X-Requested-With: XMLHttpRequest' \
+      -H 'DNT: 1' \
+      -H 'Connection: keep-alive' \
+      -H 'Referer: https://btvplus.bg/live/' \
+      -H 'Pragma: no-cache' \
+      -H 'Cache-Control: no-cache' \
+      --compressed \
+      --cookie "$cookiejar" | jq . | grep --perl-regexp --only-matching '[^"]+playlist.m3u8[^"\\]+'
+  )"
+
+  rm "$cookiejar"
+
+  mpv --fullscreen \
+    --mute=no \
+    --volume=100 \
+    --cache=10000 \
+    --title='BTV - На живо' \
+    --really-quiet \
+    "$url" &> /dev/null
+}
---
+++ functions/nova.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euxo pipefail
+
+function nova() {
+  url="$(
+    curl 'https://i.cdn.bg/live/0OmMKJ4SgY' \
+      --fail \
+      --silent \
+      -H 'Pragma: no-cache' \
+      -H 'Accept-Encoding: gzip, deflate, br' \
+      -H 'Accept-Language: bg-BG,bg;q=0.9,en;q=0.8,it;q=0.7,uk;q=0.6,mg;q=0.5,ru;q=0.4,de;q=0.3,id;q=0.2' \
+      -H 'Upgrade-Insecure-Requests: 1' \
+      -H 'User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36' \
+      -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
+      -H 'Referer: https://nova.bg/live' \
+      -H 'Connection: keep-alive' \
+      -H 'Cache-Control: no-cache' \
+      --compressed | grep --fixed-strings 'playlist.m3u8?at=' | cut --delimiter \' --fields 2
+  )"
+
+  mpv --fullscreen \
+    --mute=no \
+    --volume=100 \
+    --cache=10000 \
+    --title='NovaTV - На живо' \
+    --really-quiet \
+    "$url" &> /dev/null
+}
